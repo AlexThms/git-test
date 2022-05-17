5e2db41e89c9863f72a700fba1761ef5fb4b7ce4
AlexThms <87486756+AlexThms@users.noreply.github.com>
2022-05-17 16:46:54 +0800

    Update Git.java

--- src/main/java/com/ataojin/Git.java
+++ src/main/java/com/ataojin/Git.java
@@ -9,5 +9,6 @@ public class Git {
         System.out.println("hot fix test");
         System.out.println("push test");
         System.out.println("ssh push test");
+        System.out.println("pull test");
     }
 }
